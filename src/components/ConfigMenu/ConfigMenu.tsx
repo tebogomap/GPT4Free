28b3eeb302e0adfbeb24ee8870516226f0ddde58
tebogomap <136330397+tebogomap@users.noreply.github.com>
2024-01-28 20:46:08 +0200

    Update ConfigMenu.tsx

--- src/components/ConfigMenu/ConfigMenu.tsx
+++ src/components/ConfigMenu/ConfigMenu.tsx
@@ -104,8 +104,7 @@ export const ModelSelector = ({
           {modelOptions.map((m) => (
             <li
               className='px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer'
-             onClick={() => {
-                 onClick={() => {
+              onClick={() => {
                 _setModel(m);
                 setDropDown(false);
               }}
