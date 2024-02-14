93ef633dbd11a849378aaf489a61cafedb4ad9d2
tebogomap <136330397+tebogomap@users.noreply.github.com>
2024-02-14 22:08:11 +0200

    Update ChatContent.tsx

--- src/components/Chat/ChatContent/ChatContent.tsx
+++ src/components/Chat/ChatContent/ChatContent.tsx
@@ -76,7 +76,19 @@ const ChatContent = () => {
               )
             ))}
           </div>
-
+<div dangerouslySetInnerHTML={{ __html: `
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3346182201667382"
+     crossorigin="anonymous"></script>
+<!-- Auto image -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3346182201667382"
+     data-ad-slot="5342788181"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>        ` }} />
           <Message
             role={inputRole}
             content=''
@@ -110,6 +122,19 @@ const ChatContent = () => {
                 <DownloadChat saveRef={saveRef} />
                 <ShareGPT />
                 <CloneChat />
+                <div dangerouslySetInnerHTML={{ __html: `
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3346182201667382"
+     crossorigin="anonymous"></script>
+<!-- Auto image -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3346182201667382"
+     data-ad-slot="5342788181"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>        ` }} />
               </div>
             )}
           </div>
