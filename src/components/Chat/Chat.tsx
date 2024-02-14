1f8a754e88a10d9670ab54be10ecf7116a045e12
tebogomap <136330397+tebogomap@users.noreply.github.com>
2024-02-14 21:12:09 +0200

    Update Chat.tsx

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -17,6 +17,18 @@ const Chat = () => {
       <MobileBar />
       <main className='relative h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1'>
         <ChatContent />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3346182201667382"
+     crossorigin="anonymous"></script>
+<!-- Auto image -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3346182201667382"
+     data-ad-slot="5342788181"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         <StopGeneratingButton />
       </main>
     </div>
