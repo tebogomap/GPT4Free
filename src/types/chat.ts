a9a7e559821c8f9e668aa1687479a59385af48a4
tebogomap <136330397+tebogomap@users.noreply.github.com>
2024-07-28 22:02:26 +0200

    Update chat.ts

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -61,10 +61,39 @@ export type ModelOptions =
   | 'gpt-3.5-turbo'
   | 'gpt-3.5-turbo-16k'
   | 'gpt-3.5-turbo-1106'
-  | 'gpt-3.5-turbo-0125';
-// | 'gpt-3.5-turbo-0301';
-// | 'gpt-4-0314'
-// | 'gpt-4-32k-0314'
+  | 'gpt-3.5-turbo-0125'
+  | 'mistralai/mistral-large'
+  | 'meta-llama/llama-3.1-405b-instruct'
+  | 'meta-llama/llama-3.1-8b-instruct'
+  | 'meta-llama/llama-3.1-70b-instruct'
+  | 'meta-llama/llama-3.1-8b-instruct:free'
+  | 'cognitivecomputations/dolphin-llama-3-70b'
+  | 'mistralai/codestral-mamba'
+  | 'mistralai/mistral-nemo'
+  | 'openai/gpt-4o-mini-2024-07-18'
+  | 'openai/gpt-4o-mini'
+  | 'qwen/qwen-2-7b-instruct'
+  | 'google/gemma-2-27b-it'
+  | 'alpindale/magnum-72b'
+  | 'nousresearch/hermes-2-theta-llama-3-8b'
+  | 'google/gemma-2-9b-it:free'
+  | 'google/gemma-2-9b-it'
+  | 'openrouter/flavor-of-the-week'
+  | 'sao10k/l3-stheno-8b'
+  | 'ai21/jamba-instruct'
+  | '01-ai/yi-large'
+  | 'nvidia/nemotron-4-340b-instruct'
+  | 'anthropic/claude-3.5-sonnet:beta'
+  | 'anthropic/claude-3.5-sonnet'
+  | 'sao10k/l3-euryale-70b'
+  | 'qwen/qwen-2-7b-instruct:free'
+  | 'microsoft/phi-3-medium-4k-instruct'
+  | 'bigcode/starcoder2-15b-instruct'
+  | 'cognitivecomputations/dolphin-mixtral-8x22b'
+  | 'qwen/qwen-2-72b-instruct'
+  | 'openchat/openchat-8b'
+  | 'nousresearch/hermes-2-pro-llama-3-8b'
+  | 'mistralai/mistral-7b-instruct-v0.3';
 
 export type TotalTokenUsed = {
   [model in ModelOptions]?: {
@@ -72,6 +101,7 @@ export type TotalTokenUsed = {
     completionTokens: number;
   };
 };
+
 export interface LocalStorageInterfaceV0ToV1 {
   chats: ChatInterface[];
   currentChatIndex: number;
